40077682c7c43a84fa03ed46c0ffba2872ad1d9c
Marcus <marcus.shimotsu@gmail.com>
2021-11-09 17:20:02 -0600

    commit tree 08ffeb1dd1a2ea66928d29607abe7d48659edef9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name,email) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.innerHTML+=data.id;
+    })
+    .catch(error => {
+        document.body.querySelector('script').innerHTML = error.message;
+        console.log(document.body.innerHTML)
+    });
+}
